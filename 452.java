3bb8c43dfdf29b80d70830756c0a762f663b966b
jenniferyuaggie <24554359+jenniferyuaggie@users.noreply.github.com>
2021-03-17 20:46:04 -0500

    Create 452.java

---
+++ 452.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] i1, int[] i2) {
+                return i1[0] - i2[0];
+            } 
+        });
+        
+        int end = points[0][1];
+        int min = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] <= end) {
+                end = Math.min(end, points[i][1]);
+            } else {
+                end = points[i][1];
+                min++;
+            }
+        }
+        
+        return min;
+    }
+}
